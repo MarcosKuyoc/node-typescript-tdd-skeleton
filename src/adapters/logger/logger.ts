9861e309673cbfa86c553d7bda01a3cba077f3eb
Marcos Kuyoc <marcos.kuyoc.dev@gmail.com>
2023-01-22 17:35:47 -0600

    chore: se agregan mas configuraciones al logger

--- src/adapters/logger/logger.ts
+++ src/adapters/logger/logger.ts
@@ -1,25 +1,57 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { v4 as uuidv4 } from 'uuid';
 import pino from 'pino-http';
-// import {Request} from 'express';
 
-let logger = pino();
-if (process.env.NODE_ENV === 'Development') {
-  logger = pino({
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        translateTime: 'SYS:dd-mm-yyyy HH:mm:ss',
-        colorize: true,
-        messageKey: 'message'
+const logger = pino({
+  transport:
+    process.env.NODE_ENV === 'Development' ?
+      {
+        target: 'pino-pretty',
+        options: {
+          translateTime: 'SYS:dd-mm-yyyy HH:mm:ss',
+          colorize: true,
+          messageKey: 'message',
+        },
       }
-    },
-    messageKey: 'message',
-    autoLogging: true,
-    serializers: {
-      req: (req) => {
-        return `${req.method} ${req.url} ${req.body}, ${req.remoteAddress}`;
-      },
+      : undefined,
+  genReqId: function (req: any, res: any) {
+    if (req.id) return req.id;
+    let id = req.get('X-Correlation-Id')
+    if (id) return id
+    id = uuidv4();
+    res.header('X-Correlation-Id', id)
+    return id;
+  },
+  messageKey: 'message',
+  customProps: function (req: any) {
+    return {
+      correlationId: req['X-Correlation-Id'],
     }
-  });
-}
+  },
+  autoLogging: false,
+  serializers: {
+    req: (req) => {
+      logger.logger.info({
+        request: {
+          correlationId: req.id,
+          remoteAddress: req.remoteAddress,
+          method: req.method,
+          url: req.ur
+        }
+      });
+      if (req.method !== 'GET') {
+        logger.logger.info({
+          payload: req.raw.body
+        });
+      }
+      return undefined;
+    },
+    res: (res) => {
+      logger.logger.info({ status: res.statusCode });
+      return undefined;
+    },
+  }
+});
+
 
 export default logger;
