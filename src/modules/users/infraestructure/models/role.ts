14671dcdc9d73ba0b10b3640e182a612f844162c
Marcos Kuyoc <marcos.kuyoc.dev@gmail.com>
2023-02-22 19:37:55 -0600

    feat: se agrega el modelo del Rol

--- src/modules/users/infraestructure/models/role.ts
+++
@@ -1,9 +0,0 @@
-import { getModelForClass, prop } from '@typegoose/typegoose';
-
-export class Role {
-  @prop()
-    name: string | undefined;
-}
-
-const RolModel = getModelForClass(Role);
-export default RolModel;
